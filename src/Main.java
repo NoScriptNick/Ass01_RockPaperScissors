8622c258265998e1b227c63ed01f0460ce7eba21
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2023-10-12 14:06:58 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,63 +8,67 @@ public class Main {
         String playerTwo;
         boolean doneOne = false;
         boolean doneTwo = false;
+        boolean doneThree = false;
         String continueYN;
 
         //play prompt
-        System.out.println("Would you like to play? Type [Y/N]: ");
-        continueYN = scan.nextLine();
-        continueYN = continueYN.toUpperCase();
-        if (continueYN.equals("Y")) {
-            //loops until player 1 gives a valid response(R, P, or S)
-            do {
-                System.out.println("Player 1, what's your move? [R, P, or S]: ");
-                playerOne = scan.nextLine();
-                playerOne = playerOne.toUpperCase();
-                if (playerOne.equals("R") || playerOne.equals("P") || playerOne.equals("S")) {
-                    doneOne = true;
-                } else {
-                    System.out.println("Invalid Response");
-                }
-            } while (!doneOne);
+        do {
+            System.out.println("Would you like to play? Type [Y/N]: ");
+            continueYN = scan.nextLine();
+            continueYN = continueYN.toUpperCase();
+            if (continueYN.equals("Y")) {
+                //loops until player 1 gives a valid response(R, P, or S)
+                do {
+                    System.out.println("Player 1, what's your move? [R, P, or S]: ");
+                    playerOne = scan.nextLine();
+                    playerOne = playerOne.toUpperCase();
+                    if (playerOne.equals("R") || playerOne.equals("P") || playerOne.equals("S")) {
+                        doneOne = true;
+                    } else {
+                        System.out.println("Invalid Response");
+                    }
+                } while (!doneOne);
 
-            //loops until player 2 gives a valid response(R, P, or S)
-            do {
-                System.out.println("Player 2, what's your move? [R, P, or S]: ");
-                playerTwo = scan.nextLine();
-                playerTwo = playerTwo.toUpperCase();
-                if (playerTwo.equals("R") || playerTwo.equals("P") || playerTwo.equals("S")) {
-                    doneTwo = true;
-                } else {
-                    System.out.println("Invalid Response");
-                }
-            } while (!doneTwo);
+                //loops until player 2 gives a valid response(R, P, or S)
+                do {
+                    System.out.println("Player 2, what's your move? [R, P, or S]: ");
+                    playerTwo = scan.nextLine();
+                    playerTwo = playerTwo.toUpperCase();
+                    if (playerTwo.equals("R") || playerTwo.equals("P") || playerTwo.equals("S")) {
+                        doneTwo = true;
+                    } else {
+                        System.out.println("Invalid Response");
+                    }
+                } while (!doneTwo);
 
-            //WHO WINS?????
-            if (playerOne.equals("R") && playerTwo.equals("S")) {
-                System.out.println("Rock breaks scissors!");
-                System.out.println("\nPlayer one wins!");
-            } else if (playerOne.equals("R") && playerTwo.equals("P")) {
-                System.out.println("Paper covers rock!");
-                System.out.println("\nPlayer two wins!");
-            } else if (playerOne.equals("P") && playerTwo.equals("R")) {
-                System.out.println("Paper covers rock!");
-                System.out.println("\nPlayer one wins!");
-            } else if (playerOne.equals("P") && playerTwo.equals("S")) {
-                System.out.println("Scissors cuts paper!");
-                System.out.println("\nPlayer two wins!");
-            } else if (playerOne.equals("S") && playerTwo.equals("R")) {
-                System.out.println("Rock breaks scissors!");
-                System.out.println("\nPlayer two wins!");
-            } else if (playerOne.equals("S") && playerTwo.equals("P")) {
-                System.out.println("Scissors cuts paper!");
-                System.out.println("\nPlayer one wins!");
-            } else if (playerOne.equals(playerTwo)) {
-                System.out.println("It's a tie!");
+                //WHO WINS?????
+                if (playerOne.equals("R") && playerTwo.equals("S")) {
+                    System.out.println("Rock breaks scissors!");
+                    System.out.println("\nPlayer one wins!");
+                } else if (playerOne.equals("R") && playerTwo.equals("P")) {
+                    System.out.println("Paper covers rock!");
+                    System.out.println("\nPlayer two wins!");
+                } else if (playerOne.equals("P") && playerTwo.equals("R")) {
+                    System.out.println("Paper covers rock!");
+                    System.out.println("\nPlayer one wins!");
+                } else if (playerOne.equals("P") && playerTwo.equals("S")) {
+                    System.out.println("Scissors cuts paper!");
+                    System.out.println("\nPlayer two wins!");
+                } else if (playerOne.equals("S") && playerTwo.equals("R")) {
+                    System.out.println("Rock breaks scissors!");
+                    System.out.println("\nPlayer two wins!");
+                } else if (playerOne.equals("S") && playerTwo.equals("P")) {
+                    System.out.println("Scissors cuts paper!");
+                    System.out.println("\nPlayer one wins!");
+                } else if (playerOne.equals(playerTwo)) {
+                    System.out.println("It's a tie!");
+                }
+            } else if (continueYN.equals("N")) {
+                System.out.println("Then why are you here buddy");
+                doneThree = true;
+            } else {
+                System.out.println("Invalid Response");
             }
-        } else if (continueYN.equals("N")) {
-            System.out.println("Then why are you here buddy");
-        } else {
-            System.out.println("Invalid Response");
-        }
+        } while (!doneThree);
     }
 }
